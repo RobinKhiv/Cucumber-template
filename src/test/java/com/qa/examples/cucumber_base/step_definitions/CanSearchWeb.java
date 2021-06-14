bd991e9007638d20a0198099e286d8803bc4b600
Robin Khiv <robinkhiv@gmail.com>
2021-06-14 13:16:43 -0700

    rk:added step definition template for web search

---
+++ src/test/java/com/qa/examples/cucumber_base/step_definitions/CanSearchWeb.java
@@ -0,0 +1,26 @@
+package com.qa.examples.cucumber_base.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CanSearchWeb {
+	
+	@Given("a search brower")
+	public void a_search_brower() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("{string} is searched")
+	public void is_searched(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("then result should be true")
+	public void then_result_should_be_true() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
